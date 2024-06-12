0fc6c7c7cc98c3de3279c9fa60f6cd521e89b710
Chaima Guerfala <guerfalachaima3@gmail.com*>
2024-06-12 20:28:21 +0100

    sign up commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogInComponent } from './log-in/log-in.component';
+import { SingUpComponent } from './sing-up/sing-up.component';
 
 @NgModule({
+  
   declarations: [
     AppComponent,
-    LogInComponent
+    LogInComponent,
+    SingUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/sing-up/sing-up.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-sing-up',
+  templateUrl: './sing-up.component.html',
+  styleUrl: './sing-up.component.css'
+})
+export class SingUpComponent {
+  name: string = '';
+  email: string = '';
+  password: string = '';
+
+  signup(signupForm: NgForm) {
+    if (signupForm.valid) {
+      // Logic to handle sign-up (e.g., send data to server)
+      console.log('Sign up successful!');
+      console.log('Name:', this.name);
+      console.log('Email:', this.email);
+      console.log('Password:', this.password);
+
+      // Reset form fields
+      signupForm.resetForm();
+    } else {
+      console.error('Form is invalid. Please fill in all fields.');
+    }
+  }
+
+}
